70aca318f07e3e3459239e3f8043da94b100cb08
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-03-31 23:54:11 +0800

    创建 SlicesResourceFormat.java

---
+++ src/cn/imaginary/toolkit/image/photoshopdocument/imageresources/resources/SlicesResourceFormat.java
@@ -0,0 +1,12 @@
+package cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources;
+
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.slices.SlicesHeader;
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.slices.SlicesResourceBlock;
+import java.io.RandomAccessFile;
+
+public class SlicesResourceFormat {
+
+    public SlicesResourceFormat() {}
+
+    public void read(RandomAccessFile rafile) {}
+}
