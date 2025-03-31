1f9f01cb7383a2569ca07ab8b271f681fc9f5da2
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-03-31 23:51:56 +0800

    创建 GridAndGuideResourceFormat.java

---
+++ src/cn/imaginary/toolkit/image/photoshopdocument/imageresources/resources/GridAndGuideResourceFormat.java
@@ -0,0 +1,12 @@
+package cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources;
+
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.gridandguide.GridAndGuideHeader;
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.gridandguide.GuideResourceBlock;
+import java.io.RandomAccessFile;
+
+public class GridAndGuideResourceFormat {
+
+    public GridAndGuideResourceFormat() {}
+
+    public void read(RandomAccessFile rafile) {}
+}
