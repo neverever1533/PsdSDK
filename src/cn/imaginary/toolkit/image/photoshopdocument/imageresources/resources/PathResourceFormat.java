c4503ce4b6124999a8db639e577e4db16d520f55
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-03-31 23:53:13 +0800

    创建 PathResourceFormat.java

---
+++ src/cn/imaginary/toolkit/image/photoshopdocument/imageresources/resources/PathResourceFormat.java
@@ -0,0 +1,10 @@
+package cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources;
+
+import java.io.RandomAccessFile;
+
+public class PathResourceFormat {
+
+    public PathResourceFormat() {}
+
+    public void read(RandomAccessFile rafile) {}
+}
