9d05c3fc57af8479ab69573d88b79e3ab5d91ae1
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-03-31 23:50:33 +0800

    创建 resources/ColorSamplersResourceFormat.java

---
+++ src/cn/imaginary/toolkit/image/photoshopdocument/imageresources/resources/ColorSamplersResourceFormat.java
@@ -0,0 +1,12 @@
+package cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources;
+
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.color.ColorSamplersHeader;
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.color.ColorSamplersResourceBlock;
+import java.io.RandomAccessFile;
+
+public class ColorSamplersResourceFormat {
+
+    public ColorSamplersResourceFormat() {}
+
+    public void read(RandomAccessFile rafile, int id) {}
+}
