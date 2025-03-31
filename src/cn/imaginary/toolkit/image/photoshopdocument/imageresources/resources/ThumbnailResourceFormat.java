77adb962805b9200dd2e61dff338ba037423e23e
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-03-31 23:55:38 +0800

    创建 ThumbnailResourceFormat.java

---
+++ src/cn/imaginary/toolkit/image/photoshopdocument/imageresources/resources/ThumbnailResourceFormat.java
@@ -0,0 +1,11 @@
+package cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources;
+
+// import cn.imaginary.toolkit.image.photoshopdocument.imageresources.resources.thumbnail.ThumbnailResourceHeader;
+import java.io.RandomAccessFile;
+
+public class ThumbnailResourceFormat {
+
+    public ThumbnailResourceFormat() {}
+
+    public void read(RandomAccessFile rafile, int id) {}
+}
