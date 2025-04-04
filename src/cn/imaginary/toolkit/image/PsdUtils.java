7ed35e2d3210be6b12b4379dd461bca8f996998d
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-04-04 15:46:04 +0800

    更新 PsdUtils.java

--- src/cn/imaginary/toolkit/image/PsdUtils.java
+++ src/cn/imaginary/toolkit/image/PsdUtils.java
@@ -5,8 +5,8 @@ import cn.imaginary.toolkit.image.photoshopdocument.FileHeader;
 import cn.imaginary.toolkit.image.photoshopdocument.ImageData;
 import cn.imaginary.toolkit.image.photoshopdocument.ImageResources;
 import cn.imaginary.toolkit.image.photoshopdocument.LayerAndMaskInformation;
+import cn.imaginary.toolkit.image.photoshopdocument.layerandmask.LayerInfo;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -19,7 +19,7 @@ public class PsdUtils {
         try {
             rafile = new RandomAccessFile(file, "r");
             read(rafile);
-        } catch (FileNotFoundException e) {} finally {
+        } catch (IOException e) {} finally {
             try {
                 rafile.close();
             } catch (IOException e) {}
@@ -38,28 +38,37 @@ public class PsdUtils {
             FileHeader fheader = new FileHeader();
             fheader.read(rafile);
             System.out.println(fheader.toString());
+            System.out.println();
 
             System.out.println("start: " + rafile.getFilePointer());
 
             ColorModeData cmdata = new ColorModeData();
             cmdata.read(rafile, fheader.getColorMode());
             System.out.println(cmdata.toString());
+            System.out.println();
 
             System.out.println("start: " + rafile.getFilePointer());
 
             ImageResources iresources = new ImageResources();
             iresources.read(rafile);
             System.out.println(iresources.toString());
+            System.out.println();
 
             System.out.println("start: " + rafile.getFilePointer());
 
             LayerAndMaskInformation laminfo = new LayerAndMaskInformation();
             laminfo.read(rafile, fheader);
+            LayerInfo linfo = laminfo.getLayerInfo();
+            System.out.println(laminfo.toString());
+            System.out.println();
 
             System.out.println("start: " + rafile.getFilePointer());
 
             ImageData idata = new ImageData();
-            idata.read(rafile, fheader);
+            idata.read(rafile, fheader, linfo);
+            System.out.println(idata.toString());
+            System.out.println();
+
             System.out.println("end: " + rafile.getFilePointer());
         } catch (IOException e) {}
     }
