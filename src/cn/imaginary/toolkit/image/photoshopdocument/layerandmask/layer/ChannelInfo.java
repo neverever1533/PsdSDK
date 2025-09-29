6a570e72c190f372fc39dca7cf703d15eaa00897
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-09-29 21:36:45 +0800

    更新 ChannelInfo.java

--- src/cn/imaginary/toolkit/image/photoshopdocument/layerandmask/layer/ChannelInfo.java
+++ src/cn/imaginary/toolkit/image/photoshopdocument/layerandmask/layer/ChannelInfo.java
@@ -2,47 +2,50 @@ package cn.imaginary.toolkit.image.photoshopdocument.layerandmask.layer;
 
 public class ChannelInfo {
 
-    public ChannelInfo() {}
-
-    private int id_Channel;
     public static int ID_Channel_Red = 0;
     public static int ID_Channel_Green = 1;
     public static int ID_Channel_Blue = 2;
     public static int ID_Channel_Alpha = -1;
     public static int ID_Channel_Mask = -2;
+    private int id_Channel;
+    private long length_;
+    private long length_Data;
+    private byte[] arr_Data;
 
-    private long length_ChannelData;
+    public ChannelInfo() {}
 
-    private byte[] arr_DataCompressed;
-
-    public int getChannelID() {
+    public int getID() {
         return id_Channel;
     }
 
-    public long getChannelDataLength() {
-        return length_ChannelData;
-    }
-
-    public byte[] getChannelDataCompressed() {
-        return arr_DataCompressed;
-    }
-
-    public void setChannelID(int id) {
+    public void setID(int id) {
         id_Channel = id;
     }
 
-    public void setChannelDataLength(long length) {
-        length_ChannelData = length;
+    public long getLength() {
+        return length_;
     }
 
-    public void setChannelDataCompressed(byte[] array) {
-        arr_DataCompressed = array;
+    public long getDataLength() {
+        return length_Data;
+    }
+
+    public void setDataLength(long length) {
+        length_Data = length;
+    }
+
+    public byte[] getData() {
+        return arr_Data;
+    }
+
+    public void setData(byte[] array) {
+        arr_Data = array;
     }
 
     public String toString() {
         StringBuilder sbuilder = new StringBuilder();
         sbuilder.append("Channel ID: " + id_Channel);
-        sbuilder.append("/Channel Data Length: " + length_ChannelData);
+        sbuilder.append("/Channel Data Length: " + length_Data);
         return sbuilder.toString();
     }
 }
