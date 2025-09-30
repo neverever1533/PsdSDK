2d90b0ce87fd09daad68f76aa136f0eed6a3bad4
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-09-30 19:46:02 +0800

    更新 GlobalLayerMaskInfo.java

--- src/cn/imaginary/toolkit/image/photoshopdocument/layerandmask/GlobalLayerMaskInfo.java
+++ src/cn/imaginary/toolkit/image/photoshopdocument/layerandmask/GlobalLayerMaskInfo.java
@@ -1,27 +1,29 @@
 package cn.imaginary.toolkit.image.photoshopdocument.layerandmask;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class GlobalLayerMaskInfo {
 
-    //4.3 Global Layer Mask Info
-    public GlobalLayerMaskInfo() {}
-
-    private int length_GlobalLayerMaskInfo;
+    private int length_;
+    private int length_Data;
     private int overlayColorSpace;
-
     private int red;
     private int green;
     private int blue;
     private int alpha;
-
     private int opacity;
     private int kind;
     private int filler;
+    private int length_Filler;
+
+    //4.3 Global Layer Mask Info
+    public GlobalLayerMaskInfo() {}
 
     public int getLength() {
-        return 4 + length_GlobalLayerMaskInfo;
+        return length_;
     }
 
     public int getOverlayColorSpace() {
@@ -56,43 +58,74 @@ public class GlobalLayerMaskInfo {
         try {
             long location = rafile.getFilePointer();
 
-            //4.3.1 Global Layer Mask Info Length:4
-            // Length of global layer mask info section.
-            length_GlobalLayerMaskInfo = rafile.readInt();
+            readDataLength(rafile);
+            readData(rafile);
 
-            //4.3.2 Overlay Color Space(undocumented）：2
-            overlayColorSpace = rafile.readShort();
-
-            //4.3.3 Color Components:8
-            // 4 * 2 byte color components
-            red = rafile.readShort();
-            green = rafile.readShort();
-            blue = rafile.readShort();
-            alpha = rafile.readShort();
-
-            //4.3.4 Opacity:2
-            // Opacity. 0 = transparent, 100 = opaque.
-            opacity = rafile.readShort();
-
-            //4.3.5 Kind：1
-            // Kind. 0 = Color selected--i.e. inverted; 1 = Color protected;128 = use value stored per layer. This value is preferred. The others are for backward compatibility with beta versions.
-            kind = rafile.readByte() & 0xFF;
-
-            //4.3.6 Filler: zeros：?
-            int length_Filler = length_GlobalLayerMaskInfo - 2 - 8 - 2 - 1;
-            if (length_Filler > 0) {
-                byte[] arr = new byte[length_Filler];
-                rafile.read(arr);
-                System.out.println("filler: ");
-                for (int i = 0; i < arr.length; i++) {
-                    System.out.println(arr[i]);
-                }
-            }
-            // rafile.skipBytes(length_Filler);
-            // filler = 0;
-
+            length_ += length_Data;
+            System.out.println("global layer mask info space: " + (location + length_ - rafile.getFilePointer()));
             rafile.seek(location + getLength());
-        } catch (IOException e) {}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readData(RandomAccessFile rafile) throws IOException {
+        if (length_Data > 0) {
+            readDataArray(rafile);
+        }
+    }
+
+    public void readDataArray(RandomAccessFile rafile) throws IOException {
+        readOverlayColorSpace(rafile);
+        readColorComponents(rafile);
+        readOpacity(rafile);
+        readKind(rafile);
+        readFiller(rafile);
+    }
+
+    private void readFiller(RandomAccessFile rafile) throws IOException {
+        //4.3.6 Filler: zeros：?
+        length_Filler = length_Data - length_;
+
+        rafile.skipBytes(length_Filler);
+        filler = 0;
+    }
+
+    private void readKind(RandomAccessFile rafile) throws IOException {
+        //4.3.5 Kind：1
+        // Kind. 0 = Color selected--i.e. inverted; 1 = Color protected;128 = use value stored per layer. This value is preferred. The others are for backward compatibility with beta versions.
+        kind = rafile.readByte() & 0xFF;
+        length_ += 1;
+    }
+
+    private void readOpacity(RandomAccessFile rafile) throws IOException {
+        //4.3.4 Opacity:2
+        // Opacity. 0 = transparent, 100 = opaque.
+        opacity = rafile.readShort();
+        length_ += 2;
+    }
+
+    private void readColorComponents(RandomAccessFile rafile) throws IOException {
+        //4.3.3 Color Components:8
+        // 4 * 2 byte color components
+        red = rafile.readShort();
+        green = rafile.readShort();
+        blue = rafile.readShort();
+        alpha = rafile.readShort();
+        length_ += 8;
+    }
+
+    private void readOverlayColorSpace(RandomAccessFile rafile) throws IOException {
+        //4.3.2 Overlay Color Space(undocumented）：2
+        overlayColorSpace = rafile.readShort();
+        length_ += 2;
+    }
+
+    private void readDataLength(RandomAccessFile rafile) throws IOException {
+        //4.3.1 Global Layer Mask Info Length:4
+        // Length of global layer mask info section.
+        length_Data = rafile.readInt();
+        length_ += 4;
     }
 
     public String toString() {
